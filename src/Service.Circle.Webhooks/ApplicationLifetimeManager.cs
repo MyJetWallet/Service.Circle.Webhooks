4c652cc4854f9923dad0ca2b523a7f06f85edbf1
Dmitrii Krasovskikh <dmitry.krasovskih@gmail.com>
2021-10-07 12:43:17 +0300

    - fix;

--- src/Service.Circle.Webhooks/ApplicationLifetimeManager.cs
+++ src/Service.Circle.Webhooks/ApplicationLifetimeManager.cs
@@ -1,32 +1,40 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using MyJetWallet.Sdk.Service;
+using MyServiceBus.TcpClient;
 
 namespace Service.Circle.Webhooks
 {
     public class ApplicationLifetimeManager : ApplicationLifetimeManagerBase
     {
         private readonly ILogger<ApplicationLifetimeManager> _logger;
+        private readonly MyServiceBusTcpClient _busTcpClient;
 
-        public ApplicationLifetimeManager(IHostApplicationLifetime appLifetime, ILogger<ApplicationLifetimeManager> logger)
+        public ApplicationLifetimeManager(IHostApplicationLifetime appLifetime,
+            ILogger<ApplicationLifetimeManager> logger, MyServiceBusTcpClient busTcpClient)
             : base(appLifetime)
         {
             _logger = logger;
+            _busTcpClient = busTcpClient;
         }
 
         protected override void OnStarted()
         {
-            _logger.LogInformation("OnStarted has been called.");
+            _logger.LogInformation("OnStarted has been called");
+            _busTcpClient.Start();
+            _logger.LogInformation("MyServiceBusTcpClient is started");
         }
 
         protected override void OnStopping()
         {
-            _logger.LogInformation("OnStopping has been called.");
+            _logger.LogInformation("OnStopping has been called");
+            _busTcpClient.Stop();
+            _logger.LogInformation("MyServiceBusTcpClient is stop");
         }
 
         protected override void OnStopped()
         {
-            _logger.LogInformation("OnStopped has been called.");
+            _logger.LogInformation("OnStopped has been called");
         }
     }
 }
